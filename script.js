5049f7e9f9a3be50e3a0f42436db9bf09c12eb56
MusQho <muhammaddhuhaqolby@gmail.com>
2026-06-02 13:22:52 +0700

    menghubungkan dengan javascript dan menambahkan aksi kilik pertama

---
+++ script.js
@@ -0,0 +1,5 @@
+// mengambil element html ById
+const tombol = document.getElementById ("tombolKontak");
+tombol.addEventListener("click", function(){
+    alert("Connecting you to My Github Profile ...")
+})
