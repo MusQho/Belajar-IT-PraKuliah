4fc24d64d171342b258df3439337f691cdb05a99
MusQho <muhammaddhuhaqolby@gmail.com>
2026-06-02 14:20:30 +0700

    Menambahkan fungsi apabila diklik akan masuk ke link web yang dituju

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // mengambil element html ById
 const tombol = document.getElementById ("tombolKontak");
+// memanggil variable tombol lalu menggunakan fungsi dengan titik (addEventListener("click") = apabila di klik)
 tombol.addEventListener("click", function(){
-    alert("Connecting you to My Github Profile ...")
-})
+    // memunculkan notif pesan di web
+    alert("Connecting you to My Github Profile ..."); 
+    // // memindahkan halaman di tab yang berbeda
+    // // memindahkan halaman di tab yang sama
+    // // memajukan ke web/halaman setelahnya
+    // // memundurkan ke web/halaman sebelumnya
+    // // membaca ukuran layar secara real-time
+    // // Otomatis Geser layar sperti di scroll secara halus(smooth)
+
+    window.open("https://github.com/MusQho", "_blank");
+    // window.location.href = "https://github.com/MusQho";
+    // window.history.forward();
+    // window.history.back();
+    // window.innerHeight;
+    // window.innerWidth;
+    // window.scrollTo({ top: 0, behavior: "smooth"});
+});
