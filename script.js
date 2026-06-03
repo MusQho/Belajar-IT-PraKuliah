cfea2da940c6343a25cfc40dc499b6f8cc956760
MusQho <muhammaddhuhaqolby@gmail.com>
2026-06-03 21:56:51 +0700

    Belajar membungkus logika ke dalam fungsi kustom

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ const hourlyRate = 20;
 const button = document.getElementById("countButton");
 const cost = document.getElementById("totalCost");
 
-button.addEventListener("click", function () {
-    workHours++
-    console.log(workHours)
+// as a reuseable block of code to perform a spesific task
+function updateInterface(){
     const costTotal = workHours * hourlyRate
     cost.innerText = "$" + costTotal
-    if (workHours > 4) {
+    if (workHours > 4) 
         //we use querySelctor because we have ( class=".profile-card" ) in HTML not a Id 
-        document.querySelector(".profile-card").style.backgroundColor = "#e6f4ea"
-    }
+    document.querySelector(".profile-card").style.backgroundColor = "#e6f4ea"
+};
+
+button.addEventListener("click", function () {
+    workHours++
+    // we just call a name function 
+    updateInterface()
 });
+
+/* --- Why we not use a Return ---
+because in this program there is no task to
+process data again outside the function*/
